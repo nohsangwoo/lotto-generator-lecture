36a2c6f0fd44d5e4f5840650e297c01ea77da209
nohsangwoo <nsgr12@naver.com>
2024-12-07 03:48:32 +0900

    feat: ssr and SCR 간단설명

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
 "use client";
-import { useState } from "react";
 
 export default function Home() {
-    // useState 선언.
-    const [count, setCount] = useState(0);
-    const [text, setText] = useState("");
+    console.log("Hello from Client-Side!");
     return (
         <div className="border">
-            <div>
-                <div>카운트: {count}</div>
-                <button onClick={() => setCount(count + 1)}>증가 버튼</button>
-                <button onClick={() => setCount(count - 1)}>감소 버튼</button>
-            </div>
-            <div>
-                <div>입력된 텍스트: {text}</div>
-                <input
-                    className="border"
-                    type="text"
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                />
-            </div>
+            <h1>check the browser console and your terminal.</h1>
         </div>
     );
 }
