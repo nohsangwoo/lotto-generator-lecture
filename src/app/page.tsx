362ed63937e8102fcf53c42e119f04ce2ca84c35
nohsangwoo <nsgr12@naver.com>
2024-12-21 20:17:47 +0900

    feat: 로딩 문구 랜덤화: useEffect와 cleanup의 활용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,65 @@
 'use client'
 import { motion } from 'framer-motion'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const loadingPhrases = [
+  '번호 생성중',
+  '우주의 기운을 모으는중',
+  '행운의 돼지와 기도하는중',
+  '로또의 여신에게 부탁하는중',
+  '행운의 별자리 찾는중',
+  '네잎클로버 찾는중',
+  '황금 토끼발 문지르는중',
+  '행운의 룰렛 돌리는중',
+  '복권 요정 소환중',
+  '행운의 주문 외우는중',
+  '당첨 확률 계산중',
+  '행운의 동전 뒤집는중',
+  '로또 구슬 닦는중',
+  '당첨 번호 예측중',
+  '행운의 염주 만지는중',
+  '부자되는 주문 외우는중',
+  '로또 명당 찾는중',
+  '행운의 부적 충전중',
+  '당첨 확신 다지는중',
+  '로또 여신과 교신중',
+  '행운의 제비 뽑는중',
+  '당첨 운세 보는중',
+  '로또 신에게 기도중',
+  '행운의 주사위 굴리는중',
+  '당첨 꿈 해몽하는중',
+  '로또 점괘 보는중',
+  '행운의 카드 뽑는중',
+  '당첨 에너지 충전중',
+  '로또 마법 시전중',
+  '행운의 별똥별 찾는중',
+  '당첨 징조 분석중',
+  '로또 여행 떠나는중',
+  '행운의 거북이와 달리기중',
+  '당첨 확신 100% 충전중',
+  '로또 요정과 대화중',
+  '행운의 비밀 레시피 만드는중',
+  '당첨 확률 높이는중',
+  '로또 명상 중',
+  '행운의 주파수 맞추는중',
+  '당첨의 문 두드리는중',
+]
 
 export default function Home() {
   const [count, setCount] = useState(1)
   const [numbers, setNumbers] = useState<number[][]>([])
   const [isAnimating, setIsAnimating] = useState(false)
-  const [loadingPhrase, setLoadingPhrase] = useState('로또 번호 생성중...')
+  const [loadingPhrase, setLoadingPhrase] = useState('')
   const [dots, setDots] = useState('...')
 
   const generateLottoNumbers = () => {
     setIsAnimating(true)
+
+    // 초기 문구 설정
+    setLoadingPhrase(
+      loadingPhrases[Math.floor(Math.random() * loadingPhrases.length)],
+    )
+
     const animationDuration = Math.random() * 2000 + 1000 // 1초 ~ 3초 사이의 랜덤 애니메이션 지속 시간
     setTimeout(() => {
       const newNumbers = []
@@ -27,6 +76,23 @@ export default function Home() {
       setIsAnimating(false)
     }, animationDuration)
   }
+
+  useEffect(() => {
+    let phraseInterval: NodeJS.Timeout
+
+    // 1.5초마다 문구 변경
+    if (isAnimating) {
+      phraseInterval = setInterval(() => {
+        setLoadingPhrase(
+          loadingPhrases[Math.floor(Math.random() * loadingPhrases.length)],
+        )
+      }, 1500)
+    }
+
+    return () => {
+      clearInterval(phraseInterval)
+    }
+  }, [isAnimating])
 
   return (
     <div className="min-h-screen bg-gray-900 p-4 sm:p-8 flex flex-col items-center justify-center">
